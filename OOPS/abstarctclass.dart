72a0188b81af2eb2105d91e3e0370a8d5b7cf19c
kaviya-unwita <kaviya@unwita.com>
2025-08-22 11:14:03 +0530

    abstract class

--- OOPS/abstarctclass.dart
+++ OOPS/abstarctclass.dart
@@ -1,8 +1,12 @@
 abstract class Shape {
-  void draw();
+  void draw(); //abstract method
+ 
+  int areaOfSqure(int a, int b) { //concrete method
+    return a * b;
+  }
 }
 
-class Circle implements Shape {
+class Circle extends Shape {
   @override
   void draw() => print("Drawing Circle");
 }
@@ -10,4 +14,5 @@ class Circle implements Shape {
 void main() {
   var Myobj = Circle();
   Myobj.draw();
+  print(Myobj.areaOfSqure(5, 3));
 }
