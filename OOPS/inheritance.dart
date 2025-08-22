35b013cffbba8387dcaddb8ad76f29240038ccad
kaviya-unwita <kaviya@unwita.com>
2025-08-22 10:11:56 +0530

    Inheritance

--- OOPS/inheritance.dart
+++ OOPS/inheritance.dart
@@ -1,13 +1,31 @@
 class Animal {
-  void eat() => print("Animal is eating");
+  final String type;
+
+  Animal(String type) : this.type = type;
+
+  void eat() => print("Animal is eating from parent ");
+
+  void display() {
+    print(type);
+  }
 }
 
 class Dog extends Animal {
-  void bark() => print("Dog is barking");
+  final String gender;
+
+  Dog(String type, String gender) : this.gender = gender, super(type);
+  void bark() => print("Dog is barking from child");
+
+  @override
+  void display() {
+    super.display();
+    print(gender);
+  }
 }
 
 void main() {
-  var dog = Dog();
+  var dog = Dog("Male", "Domestic");
   dog.eat();
   dog.bark();
+  dog.display();
 }
